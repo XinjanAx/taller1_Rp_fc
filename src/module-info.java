f5a0dc9748e441deab7145fb2424d53adc26febb
Alex <Alex@192.168.1.19>
2022-05-17 21:17:52 -0500

    archivos necesarios para el proyecto

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module taller1_Rp_fc1 {
+}
---
+++ src/taller1_Rp_fc1/Main.java
@@ -0,0 +1,10 @@
+package taller1_Rp_fc1;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("mi primer poyecto en java github");
+	}
+
+}
